88006eebbf044b3f27f0bfbf5e1fd793b63c8dc6
unknown <greeeensfreem@gmail.com>
2022-04-23 16:57:53 +0900

    引数で渡した値を元にカラムを決定する

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import * as clipboard from 'clipboardy'
+import getColumnNumFromArg from './helper/getColumnNumFromArg.js'
 import makeOutput from './helper/makeOutput.js'
 
 const main = (): void => {
+  const columnNum = getColumnNumFromArg()
+
   const c = clipboard.readSync()
-
   const arr = c.split(/\r\n/)
-  console.log(arr)
 
-
-  const output = makeOutput(arr)
+  const output = makeOutput(arr, columnNum)
 
   console.log(output)
 }
